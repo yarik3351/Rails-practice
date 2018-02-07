f74b5761c7c32570bb55f636caba9df4519266f7
yarik3351 <yarik3351@gmail.com>
2018-02-07 13:45:24 +0200

    Chapter 3 variables

---
+++ 3/variables.rb
@@ -0,0 +1,16 @@
+myString = '...ты можешь сказать это снова...'
+puts myString
+puts myString
+
+name = 'Патриция Розанна Джессика Милдред Оппенгеймер'
+puts 'Меня зовут ' + name + '.'
+puts 'Ого!  ' + name + ' - это правда длинное имя!'
+
+composer = 'Mozart'
+puts composer + ' was a "sensation" in his time.'
+composer = 'Бетховен'
+puts 'Но мне лично больше нравится ' + composer + '.'
+var = 'this is one'+' string' 
+puts var
+var = 5 * (1+2)
+puts var
